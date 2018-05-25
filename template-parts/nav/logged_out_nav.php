bd6a24ebdfe13f38226b04a54105593a6b7ccb7e
Jeremy Alexander <jwalexander22@gmail.com>
2018-05-25 04:14:46 +0000

    bloginfo

--- template-parts/nav/logged_out_nav.php
+++ template-parts/nav/logged_out_nav.php
@@ -1,7 +1,7 @@
 <div data-collapse="medium" data-animation="default" data-duration="400" class="navbar w-nav">
 <div class="w-container">
 <a href="/" class="brand w-nav-brand">
-  <h3 class="brand-text"><strong>we make</strong> <em>indie games</em></h3>
+  <h3 class="brand-text"><?php bloginfo( 'name' ); ?></h3>
 </a>
 <nav role="navigation" class="w-nav-menu">
    <?php get_template_part( 'template-parts/nav/nav_out'); ?>   
