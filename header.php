3b6cc38e30b4c5ff0175cc3dafeb929bc0a98c9c
Jeremy Alexander <jwalexander22@gmail.com>
2018-05-25 05:17:13 +0000

    remove my drip tag

--- header.php
+++ header.php
@@ -17,21 +17,7 @@
 
     <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
 
-<!-- Drip -->
-<script type="text/javascript">
-  var _dcq = _dcq || [];
-  var _dcs = _dcs || {};
-  _dcs.account = '1371089';
 
-  (function() {
-    var dc = document.createElement('script');
-    dc.type = 'text/javascript'; dc.async = true;
-    dc.src = '//tag.getdrip.com/1371089.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(dc, s);
-  })();
-</script>
-<!-- end Drip -->
 
     <?php wp_head(); ?>
     
