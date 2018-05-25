461a1e0fc8045019b0561faade8837e5da35ad94
Jeremy Alexander <jwalexander22@gmail.com>
2018-05-25 04:07:42 +0000

    navbars

---
+++ template-parts/nav/logged_in_nav.php
@@ -0,0 +1,13 @@
+<div data-collapse="medium" data-animation="default" data-duration="400" class="navbar w-nav">
+<div class="w-container">
+<a href="#" class="brand w-nav-brand">
+  <h3 class="brand-text"><strong>we make</strong> <em>indie games</em></h3>
+</a>
+<nav role="navigation" class="w-nav-menu">
+    <?php get_template_part( 'template-parts/nav/nav_in'); ?>   
+</nav>
+<div class="menu-button w-nav-button">
+  <div class="w-icon-nav-menu"></div>
+</div>
+</div>
+</div>
