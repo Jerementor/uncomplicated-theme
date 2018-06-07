abd6424bc0c6fa4f7ac9cd62222aeba556de448c
Jeremy Alexander <jwalexander22@gmail.com>
2018-06-07 23:21:10 +0000

    add aside to sidebar

--- template-parts/course/course-sidebar.php
+++ template-parts/course/course-sidebar.php
@@ -1,8 +1,9 @@
 <div class="course-sidebar">
-         <?php if(is_active_sidebar('course-sidebar')) : ?>
+         <!--<!?php if(is_active_sidebar('course-sidebar')) : ?>-->
+        <aside id="secondary" class="widget-area" role="complementary">
             <?php dynamic_sidebar('course-sidebar'); ?>
-        <?php endif; ?>
-        <!--<aside id="secondary" role="complementary">-->
-       
+        </aside>
+        <!--<!?php endif; ?>-->
+
         <!--</aside>   -->
 </div>
