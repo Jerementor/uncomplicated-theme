58cc7d1f5be226514db5f956e5b0b5bec40324a9
Jeremy Alexander <jwalexander22@gmail.com>
2018-06-07 21:04:50 +0000

    remove aside from sidebar

--- template-parts/course/course-sidebar.php
+++ template-parts/course/course-sidebar.php
@@ -1,7 +1,8 @@
 <div class="course-sidebar">
-        <aside id="secondary" role="complementary">
-                <?php if(is_active_sidebar('course-sidebar')) : ?>
-                    <?php dynamic_sidebar('course-sidebar'); ?>
-                <?php endif; ?>
-        </aside>   
+         <?php if(is_active_sidebar('course-sidebar')) : ?>
+            <?php dynamic_sidebar('course-sidebar'); ?>
+        <?php endif; ?>
+        <!--<aside id="secondary" role="complementary">-->
+       
+        <!--</aside>   -->
 </div>
