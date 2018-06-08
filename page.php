0d3ad4dd1432eb1e0fbe3ae3490c0cd869e760be
Jeremy Alexander <jwalexander22@gmail.com>
2018-06-08 03:00:59 +0000

    add edd support

--- page.php
+++ page.php
@@ -8,6 +8,11 @@ get_header();
 ?>
 <!-- Page.php -->
 
+<div class="section-minvh bg-grey-lighter">
+<div class="padding-20">
+<div class="w-container">
+<div class="get-post-content">
+<div class="mx-640">
 <?php
 if ( have_posts() ) {
 	while ( have_posts() ) {
@@ -18,5 +23,10 @@ if ( have_posts() ) {
 	} // end while
 } // end if
 ?>
+</div>
+</div>
+</div>
+</div>
+</div>
 
 <?php get_footer(); ?>
