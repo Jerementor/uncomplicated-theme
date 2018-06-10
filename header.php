c1ed7c4f2fee2d04235ef7a8b3ff21876746871c
Jeremy Alexander <jwalexander22@gmail.com>
2018-06-10 06:57:26 +0000

    remove rm

--- header.php
+++ header.php
@@ -2,17 +2,6 @@
 <html>
 <head>
     
-    <!-- RightMessage -->
-    <script type="text/javascript"> 
-    (function(p, a, n, d, o, b) {
-    o = n.createElement('script'); o.type = 'text/javascript'; o.async = true; o.src = 'https://tag.rightmessage.com/'+p+'.js';
-    b = n.getElementsByTagName('script')[0]; b.parentNode.insertBefore(o, b);
-    d = function(h, u, i) { var o = n.createElement('style'); o.id = 'rmcloak'+i; o.type = 'text/css';
-    o.appendChild(n.createTextNode('.rmcloak'+h+'{visibility:hidden}.rmcloak'+u+'{display:none}'));
-    b.parentNode.insertBefore(o, b); return o; }; o = d('', '-hidden', ''); d('-stay-invisible', '-stay-hidden', '-stay');
-    setTimeout(function() { o.parentNode && o.parentNode.removeChild(o); }, a);
-    })('1855395163', 2500, document);
-    </script>    
     <meta charset="<?php bloginfo('charset'); ?>" />
     <meta content="width=device-width, initial-scale=1" name="viewport">      
     <title><?php wp_title(''); ?></title>
