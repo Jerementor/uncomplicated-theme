4e0dd8f2081b91162cf39e706e566023f8ed2ff5
Jeremy Alexander <jwalexander22@gmail.com>
2018-06-14 03:26:54 +0000

    header/footer reconfig 1

--- header.php
+++ header.php
@@ -1,5 +1,19 @@
-<!DOCTYPE html>
-<html>
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
 <head>
 <!-- RightMessage -->
 <script type="text/javascript"> 
@@ -12,11 +26,10 @@
     setTimeout(function() { o.parentNode && o.parentNode.removeChild(o); }, a);
 })('1855395163', 2500, document);
 </script>
-    
-    <meta charset="<?php bloginfo('charset'); ?>" />
-    <meta content="width=device-width, initial-scale=1" name="viewport">      
     <title><?php wp_title(''); ?></title>
-    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
     <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
     <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js" type="text/javascript"></script>
@@ -24,8 +37,9 @@
     <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
     <!--<script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>-->
     <?php wp_head(); ?>
-</head>
 
+<?php wp_head(); ?>
+</head>
 <body <?php body_class(); ?>>
 <div class="blue-topbar"></div>
 <?php if (is_user_logged_in() ) : ?> 
