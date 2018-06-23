8fa91118c89c1a5023b4f4414efb32bbb4f77eed
Jeremy Alexander <jwalexander22@gmail.com>
2018-06-23 05:24:42 +0000

    updated articles for index.php

--- index.php
+++ index.php
@@ -12,22 +12,43 @@ get_header();
 <div class="section-minvh bg-grey-lighter">
 <div class="padding-20">
 <div class="w-container">
-  <h1><?php wp_title(''); ?></h1>
+<h1><?php wp_title(''); ?></h1>
 <div class="w-row">
     <div class="w-col w-col-8">
 	     <?php if ( have_posts() ) : ?>
     		<?php while ( have_posts() ) : the_post(); ?>
-    		   <div class="get-post-info">
-       			    <?php the_title(sprintf('<a class="article-header" href="%s" rel="bookmark">', esc_url(get_permalink() )), '</a>'); ?>
-       			    <p><?php the_excerpt(); ?></p>
+    		 <?php $author = get_the_author(); ?> 
+    		
+            <div class="article-card">
+              <div class="article-card-header"><?php the_title(sprintf('<a class="article-header" href="%s" rel="bookmark">', esc_url(get_permalink() )), '</a>'); ?></div>
+              <div class="article-card-excerpt">
+                <p><?php the_excerpt(); ?></p>
+              </div>
+              <div class="article-card-readmore bg-grey-lightest">
+                <div class="w-row">
+                  <div class="w-col w-col-2">
+                      <!--<img src="images/jer3.jpg" width="48" class="post-avatar">-->
+                 </div>
+                  <div class="w-col w-col-6">
+                    <div><strong class="bold-text"><?php echo $author ?></strong></div>
+                    <div class="post-date"><?php the_time('F jS'); ?> </div>
+                  </div>
+                  <div class="w-clearfix w-col w-col-4">
+                      <a href="<?php the_permalink(); ?>" class="btn-dark float-right w-button">Read Post</a>
+                  </div>
                 </div>
+              </div>
+            </div>
+   
 	        <?php endwhile; ?>
 	    <?php endif; ?>    			
       	     
     </div>
     
     <div class="w-col w-col-4">
+        <div class="widget-card">
         <?php get_sidebar(); ?>	
+        </div>
     </div>
 </div>
 </div>
