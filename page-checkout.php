e4fbee9b817e537b182ae46e4ae195bc183d374d
Jeremy Alexander <jwalexander22@gmail.com>
2018-06-25 02:29:13 +0000

    add page-checkout template

---
+++ page-checkout.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * The template for displaying all pages
+ * Template Name: Page Checkout
+ *
+ * @since 1.0
+ */
+get_header(); 
+?>
+<!--PAGE BLANK-->
+<div class="padding-100 bg-grey-lightest">
+<div class="w-container">
+<?php 
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post(); 
+
+		the_content();
+	
+	} // end while
+} // end if
+?>
+</div>
+</div>
+<?php get_footer(); ?>
